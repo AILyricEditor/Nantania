258a065e07afff02a10b820532f1444eb26c84cd
AILyricEditor <ailyriceditor@gmail.com>
2025-12-15 11:10:55 -0500

    little changes

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-console.log("Hello, World!");
-console.log("This is a simple JavaScript program.");
-console.log("test message");
+import Phaser from "phaser";
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
